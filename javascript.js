7cc4a66a9f5eaca2323e4fbdb9fc4b11c06bc90b
ughhhmitoj <77149099+ughhhmitoj@users.noreply.github.com>
2023-03-08 07:28:20 +0530

    Lol

---
+++ javascript.js
@@ -0,0 +1,16 @@
+let userName = prompt("Who's there?", '');
+if (userName === 'Kajal'){
+    let pass = prompt ("Password?",'');
+
+    if (pass === 'Amitoj'){
+        alert("I Love You!")
+    } else if (pass === '' || pass === null){
+        alert("Wrong Password.Please Refresh this Page to Try again");
+    } else{
+        alert("Wrong Password. Please Refresh this Page to Try again");
+    }
+} else if (userName === "" || userName === null){
+    alert("Canceled. Please Refresh this Page to Try again");
+} else {
+    alert("Fuck off!" + " " + userName)
+}
