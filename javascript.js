9735cbdcc43c1551afa454687b8453810aa8b3f8
ughhhmitoj <77149099+ughhhmitoj@users.noreply.github.com>
2023-08-27 11:34:18 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
-let userName = prompt("Who's there?", '');
-if (userName === 'Kajal'){
-    let pass = prompt ("Password?",'');
+let userName = toLowerCase(prompt("Who's there?", ''));
+if (userName === 'kajal'){
+    let pass = toLowerCase(prompt ("Password?",''));
 
-    if (pass === 'Amitoj'){
+    if (pass === 'amitoj'){
         alert("I Love You!")
     } else if (pass === '' || pass === null){
         alert("Wrong Password.Please Refresh this Page to Try again");
